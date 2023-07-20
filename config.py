2a34a325e6f51f47015154868d80cf22c1e93588
Minjun Lu <luminjun1999@foxmail.com>
2023-07-20 21:48:39 +0800

    add config

---
+++ config.py
@@ -0,0 +1,22 @@
+
+import yaml
+from pprint import pprint,pformat
+
+class Config():
+    def __init__(self,path='config/config.yaml') -> None:
+        file = open(path,'r',encoding="utf-8")
+        self.file_data = file.read()
+        file.close()
+        self.config = yaml.load(self.file_data,Loader=yaml.FullLoader)
+        for k,v in self.config.items():
+            setattr(self,k,v)
+    def __call__(self):
+        return self.config
+    
+    def __str__(self):
+        return pformat(self.config)
+
+
+if __name__ == '__main__':
+    cfg =  Config()
+    print(cfg)
