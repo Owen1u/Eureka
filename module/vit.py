785dd7798e19c098d141531fb0156b34f7141f8e
Minjun Lu <luminjun1999@foxmail.com>
2023-07-21 19:46:40 +0800

    add vit

---
+++ module/vit.py
@@ -0,0 +1,178 @@
+'''
+Descripttion: 
+version: 
+Contributor: Minjun Lu
+Source: Original
+LastEditTime: 2023-07-21 19:43:52
+'''
+from functools import partial
+from typing import Callable, List, Optional,Iterable
+from collections import OrderedDict
+import torch
+import torch.nn as nn
+import math
+from torchvision.models.vision_transformer import Encoder,ConvStemConfig,ConvNormActivation
+    
+class ViTAnysize(nn.Module):
+    def __init__(
+        self, 
+        image_size: Iterable, 
+        patch_size: Iterable, 
+        num_layers: int, 
+        num_heads: int, 
+        hidden_dim: int,
+        mlp_dim: int, 
+        dropout: float = 0, 
+        attention_dropout: float = 0, 
+        num_classes: int = 1000, 
+        representation_size: int = None, 
+        norm_layer: Callable[..., torch.nn.Module] = partial(nn.LayerNorm, eps=1e-6), 
+        conv_stem_configs: Optional[List[ConvStemConfig]] = None,
+        classification: bool = True):
+        super().__init__()
+        
+        
+
+        torch._assert(image_size[-1] % patch_size[-1] == 0 and image_size[-2] % patch_size[-2] == 0, "Input shape indivisible by patch size!")
+        self.image_size = image_size
+        self.patch_size = patch_size
+        self.hidden_dim = hidden_dim
+        self.mlp_dim = mlp_dim
+        self.attention_dropout = attention_dropout
+        self.dropout = dropout
+        self.classification = classification
+        self.num_classes = num_classes
+        self.representation_size = representation_size
+        self.norm_layer = norm_layer
+
+        if conv_stem_configs is not None:
+            # As per https://arxiv.org/abs/2106.14881
+            seq_proj = nn.Sequential()
+            prev_channels = 3
+            for i, conv_stem_layer_config in enumerate(conv_stem_configs):
+                seq_proj.add_module(
+                    f"conv_bn_relu_{i}",
+                    ConvNormActivation(
+                        in_channels=prev_channels,
+                        out_channels=conv_stem_layer_config.out_channels,
+                        kernel_size=conv_stem_layer_config.kernel_size,
+                        stride=conv_stem_layer_config.stride,
+                        norm_layer=conv_stem_layer_config.norm_layer,
+                        activation_layer=conv_stem_layer_config.activation_layer,
+                    ),
+                )
+                prev_channels = conv_stem_layer_config.out_channels
+            seq_proj.add_module(
+                "conv_last", nn.Conv2d(in_channels=prev_channels, out_channels=hidden_dim, kernel_size=1)
+            )
+            self.conv_proj: nn.Module = seq_proj
+        else:
+            self.conv_proj = nn.Conv2d(
+                in_channels=3, out_channels=hidden_dim, kernel_size=patch_size, stride=patch_size
+            )
+
+        seq_length = image_size[-1] * image_size[-2] // patch_size[-1] // patch_size[-2]
+
+        # Add a class token
+        self.class_token = nn.Parameter(torch.zeros(1, 1, hidden_dim))
+        seq_length += 1
+
+        self.encoder = Encoder(
+            seq_length,
+            num_layers,
+            num_heads,
+            hidden_dim,
+            mlp_dim,
+            dropout,
+            attention_dropout,
+            norm_layer,
+        )
+        self.seq_length = seq_length
+
+        heads_layers: OrderedDict[str, nn.Module] = OrderedDict()
+        if representation_size is None:
+            heads_layers["head"] = nn.Linear(hidden_dim, num_classes)
+        else:
+            heads_layers["pre_logits"] = nn.Linear(hidden_dim, representation_size)
+            heads_layers["act"] = nn.Tanh()
+            heads_layers["head"] = nn.Linear(representation_size, num_classes)
+
+        self.heads = nn.Sequential(heads_layers)
+
+        if isinstance(self.conv_proj, nn.Conv2d):
+            # Init the patchify stem
+            fan_in = self.conv_proj.in_channels * self.conv_proj.kernel_size[0] * self.conv_proj.kernel_size[1]
+            nn.init.trunc_normal_(self.conv_proj.weight, std=math.sqrt(1 / fan_in))
+            if self.conv_proj.bias is not None:
+                nn.init.zeros_(self.conv_proj.bias)
+        elif self.conv_proj.conv_last is not None and isinstance(self.conv_proj.conv_last, nn.Conv2d):
+            # Init the last 1x1 conv of the conv stem
+            nn.init.normal_(
+                self.conv_proj.conv_last.weight, mean=0.0, std=math.sqrt(2.0 / self.conv_proj.conv_last.out_channels)
+            )
+            if self.conv_proj.conv_last.bias is not None:
+                nn.init.zeros_(self.conv_proj.conv_last.bias)
+
+        if hasattr(self.heads, "pre_logits") and isinstance(self.heads.pre_logits, nn.Linear):
+            fan_in = self.heads.pre_logits.in_features
+            nn.init.trunc_normal_(self.heads.pre_logits.weight, std=math.sqrt(1 / fan_in))
+            nn.init.zeros_(self.heads.pre_logits.bias)
+
+        if isinstance(self.heads.head, nn.Linear):
+            nn.init.zeros_(self.heads.head.weight)
+            nn.init.zeros_(self.heads.head.bias)
+            
+    def _process_input(self, x: torch.Tensor) -> torch.Tensor:
+        n, c, h, w = x.shape
+        p_h,p_w = self.patch_size[-2:]
+        torch._assert(h == self.image_size[-2], "Wrong image height!")
+        torch._assert(w == self.image_size[-1], "Wrong image width!")
+        n_h = h // p_h
+        n_w = w // p_w
+
+        # (n, c, h, w) -> (n, hidden_dim, n_h, n_w)
+        x = self.conv_proj(x)
+        # (n, hidden_dim, n_h, n_w) -> (n, hidden_dim, (n_h * n_w))
+        x = x.reshape(n, self.hidden_dim, n_h * n_w)
+
+        # (n, hidden_dim, (n_h * n_w)) -> (n, (n_h * n_w), hidden_dim)
+        # The self attention layer expects inputs in the format (N, S, E)
+        # where S is the source sequence length, N is the batch size, E is the
+        # embedding dimension
+        x = x.permute(0, 2, 1)
+
+        return x
+    
+    def forward(self, x: torch.Tensor):
+        # Reshape and permute the input tensor
+        x = self._process_input(x)
+        n = x.shape[0]
+
+        # Expand the class token to the full batch
+        batch_class_token = self.class_token.expand(n, -1, -1)
+        x = torch.cat([batch_class_token, x], dim=1)
+
+        x = self.encoder(x)
+
+        if self.classification:
+            # Classifier "token" as used by standard language architectures
+            x = x[:, 0]
+
+            x = self.heads(x)
+
+        return x
+
+if __name__=='__main__':
+    model = ViTAnysize(
+                    image_size=[3,608,1088],
+                    patch_size=[16,32],
+                    num_layers=12,
+                    num_heads=12,
+                    hidden_dim=768,
+                    mlp_dim=3072,
+                    classification=False,
+                    num_classes = 512
+                )
+    x = torch.randn([8,3,608,1088])
+    y = model(x)
+    print(y.size())
